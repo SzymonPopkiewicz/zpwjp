14a89279ce11d2f55502cd4f5e8d5a4307599ba4
SzymonPopkiewicz <szympop@student.agh.edu.pl>
2025-12-06 17:46:03 +0100

    Save my local changes

--- analysis.py
+++ analysis.py
@@ -1,22 +1,25 @@
 import pandas as pd
 import warnings
+from processing import compute_daily_returns, filter_date_range
+from data_fetcher import fetch_stock
+import numpy as np
 def summary_stats(df):
     with warnings.catch_warnings():
         warnings.simplefilter("ignore")
 
         df = df.sort_values("Date")
 
-        if df.empty:
-            return pd.DataFrame(
-                [
-                    ("Mean return", None),
-                    ("Volatility", None),
-                    ("Min price", None),
-                    ("Max price", None),
-                    ("Full return", None)
-                ],
-                columns=["Metric", "Value"]
-            )
+        # if df.empty:
+        #     return pd.DataFrame(
+        #         [
+        #             ("Mean daily return", None),
+        #             ("Volatility", None),
+        #             ("Full return", None),
+        #             ("Min price", None),
+        #             ("Max price", None)
+        #         ],
+        #         columns=["Metric", "Value"]
+        #     )
 
         # Calculate full return safely
         first = df["Close"].iloc[0]
@@ -24,14 +27,36 @@ def summary_stats(df):
         full_return = (last / first) - 1
 
         stats = {
-            "Mean return": float(df["Return"].mean()),
-            "Volatility": float(df["Return"].std()),
-            "Min price": float(df["Close"].min()),
-            "Max price": float(df["Close"].max()),
-            "Full return": float(full_return)
+            "Mean daily return": f'{df["Return"].mean() * 100:.2f}%',
+            "Volatility": f'{df["Return"].std() * 100:.2f}%',
+            "Full return": f'{full_return * 100:.2f}%',
+            "Min price": f'{df["Close"].min():.2f}',
+            "Max price": f'{df["Close"].max():.2f}'
         }
 
         return pd.DataFrame(
             [(k, v) for k, v in stats.items()],
             columns=["Metric", "Value"]
         )
+    
+def portfolio_calcs(tickers, weights, start, end):
+    portfolio_df = pd.DataFrame()
+    for ticker in tickers:
+        data = fetch_stock(ticker, str(start), str(end))
+        data = filter_date_range(data, start, end)
+        portfolio_df[ticker] = data["Close"]/data["Close"].iloc[0] * weights[ticker]
+
+    portfolio_df["Portfolio"] = portfolio_df.sum(axis=1)
+    print(portfolio_df.head())
+    portfolio_df["Return"] = portfolio_df["Portfolio"].pct_change()
+
+    # --- calculate profit (total growth from first day) ---
+    last = portfolio_df["Portfolio"].iloc[-1]
+    first = portfolio_df["Portfolio"].iloc[0]
+    profit_pct = (last / first - 1) * 100
+    volatility_daily = portfolio_df["Return"].std()*100
+
+    return last, profit_pct, volatility_daily
+
+
+
